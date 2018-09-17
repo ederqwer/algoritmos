397af21cd1fbdf069ed1dcfedd60d688a2f26d36
ederqwer <fanta_eder@hotmail.com>
2018-09-17 03:34:41 -0500

    ok

--- Arbol AVL/arbol.java
+++ Arbol AVL/arbol.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.math.*;
 public class arbol{
     nodo raiz;
     public arbol(){
@@ -7,33 +8,60 @@ public class arbol{
     public void insertar(int info){
         nodo x = new nodo(info);
         if(raiz == null) raiz = x;
-        else insertar(x, raiz, null);
+        else raiz = insertar(x, raiz);
     }
-    public nodo insertar(nodo x, nodo aux, nodo padre){
+    public nodo insertar(nodo x, nodo aux){
         if(aux == null) return x;
         if(x.info<aux.info){//izq
-            aux.izq = insertar(x, aux.izq, aux);
+            aux.izq = insertar(x, aux.izq);
             aux.w = aux.izq.w+1;
-            if(aux.factorb()<-1){//RI
-                System.out.println("Rot izq en:"+aux.info);
-            }else if(aux.factorb()>1){//RD
-                System.out.println("Rot der en:"+aux.info);
-            }
+            aux = equilibrar(aux);
         }else{ //der
-            aux.der = insertar(x, aux.der, aux);
+            aux.der = insertar(x, aux.der);
             aux.w = aux.der.w+1;
-            if(aux.factorb()<-1){//RI
-                System.out.println("Rot izq en:"+aux.info);
-            }else if(aux.factorb()>1){//RD
-                System.out.println("Rot der en:"+aux.info);
-            }
+            aux = equilibrar(aux);
         }
         return aux;
     }
 
-    private void equilibrar(nodo x, nodo padre){
-
+    private nodo equilibrar(nodo x){
+        if(x.factorb()<-1){//RI
+            System.out.print("Rot izq en: "+x.info);
+            if(x.der.factorb()>=1){
+                System.out.print(" DOBLE!");
+                x.der = RD(x.der);
+            }
+            System.out.println("");
+            return RI(x);
+        }else if(x.factorb()>1){//RD
+            System.out.print("Rot der en: "+x.info);
+            if(x.izq.factorb()<=-1){
+                x.izq = RI(x.izq);
+                System.out.print(" DOBLE!");
+            }
+            System.out.println("");
+            return RD(x);
+        } return x;
     }
+
+    private nodo RI(nodo x){
+        nodo aux = x.der;
+        x.der = aux.izq;
+        aux.izq = x;
+        x.w = Math.max(altura(x.izq), altura(x.der)) + 1;
+        aux.w = Math.max(altura(aux.der), x.w) + 1;
+        return aux;
+    }
+
+    private nodo RD(nodo x){
+        nodo aux = x.izq;
+        x.izq = aux.der;
+        aux.der=x;
+        x.w = Math.max(altura(x.izq), altura(x.der)) + 1;
+        aux.w = Math.max(altura(aux.izq), x.w) + 1;
+        return aux;
+    }
+
 
     public void mostrar(){
         Queue<nodo> q = new LinkedList<nodo>();
@@ -42,8 +70,12 @@ public class arbol{
         while(!q.isEmpty()){
             if(q.element().izq!=null) q.add(q.element().izq);
             if(q.element().der!=null) q.add(q.element().der);
-            nodo x = q.poll();
-            System.out.println(x.info+" w = "+x.w);
+            nodo n = q.poll();
+            System.out.println(n.info+" w = "+n.w);
         }
+    }
+
+    private int altura(nodo x){
+        return (x==null)?0:x.w;
     }
 }
--- Arbol AVL/main.java
+++ Arbol AVL/main.java
@@ -1,9 +1,15 @@
 public class main{
     public static void main(String[] args) {
         arbol a = new arbol();
+        a.insertar(15);
+        a.insertar(20);
+        a.insertar(17);
+        a.insertar(30);
+        a.insertar(35);
+        a.insertar(18);
+        a.insertar(11);
+        a.insertar(13);
         a.insertar(4);
-        a.insertar(10);
-        a.insertar(12);
         a.mostrar();
     }
 }
